572130a09751bb7274436020d7e266f39ffa2968
ibrainiac <franqhazard@gmail.com>
2024-06-06 23:03:36 +0300

    calyptus

---
+++ target/debug/deps/calyptus-0ae87adb9e68881b.d
@@ -0,0 +1,5 @@
+/Users/mac/rust/calyptus/calyptas/target/debug/deps/libcalyptus-0ae87adb9e68881b.rmeta: src/main.rs
+
+/Users/mac/rust/calyptus/calyptas/target/debug/deps/calyptus-0ae87adb9e68881b.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/calyptus-c1c058a69cb30c65.d
@@ -0,0 +1,5 @@
+/Users/mac/rust/calyptus/calyptas/target/debug/deps/libcalyptus-c1c058a69cb30c65.rmeta: src/main.rs
+
+/Users/mac/rust/calyptus/calyptas/target/debug/deps/calyptus-c1c058a69cb30c65.d: src/main.rs
+
+src/main.rs:
